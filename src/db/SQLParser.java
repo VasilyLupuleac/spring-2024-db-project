f1f1795dc7a9bbc8d888cfce4f34e761cb52ab98
VasilyLupuleac <v_lupuleac@mail.ru>
2024-02-28 15:07:29 +0200

    Add SQLParser interface

---
+++ src/db/SQLParser.java
@@ -0,0 +1,9 @@
+package db;
+
+import java.sql.ResultSet;
+
+public interface SQLParser {
+    public DataEntry[] parseAll(ResultSet resultSet);
+    public DataEntry parseOne(ResultSet resultSet);
+
+}
