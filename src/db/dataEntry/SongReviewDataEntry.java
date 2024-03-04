83b519acc51514779c4182702c4d594dc2d3f2b7
Melinda Pozna <melindapozna@gmail.com>
2024-03-04 16:49:18 +0200

    Rewrite DataEntries to support parsing

--- src/db/dataEntry/SongReviewDataEntry.java
+++ src/db/dataEntry/SongReviewDataEntry.java
@@ -2,6 +2,8 @@ package db.dataEntry;
 
 import db.DataEntry;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -12,11 +14,68 @@ public class SongReviewDataEntry extends DataEntry {
     String text;
     String author;
     Date date;
+    int songID;
 
-    SongReviewDataEntry (int reviewID, float rating, String text, String author, Date date) {
+    SongReviewDataEntry (int reviewID, float rating, String text, String author, Date date, int songID) {
         this.reviewID = reviewID;
         this.rating = rating;
         this.text = text;
         this.author = author;
+        this.date = date;
+        this.songID = songID;
     }
+    public String getField(String label) {
+        switch (fieldIndexMap.get(label)) {
+            case 0:
+                return Integer.toString(reviewID);
+            case 1:
+                return Float.toString(rating);
+            case 2:
+                return text;
+            case 3:
+                return author;
+            case 4:
+                DateFormat df = new SimpleDateFormat("yyyy-mm-dd");
+                return df.format(date);
+            case 5:
+                return Integer.toString(songID);
+        }
+        return null;
+    }
+
+    protected String[] fields = new String[]{"reviewID", "rating", "text", "author", "date", "songID"};
+
+    public SongReviewDataEntry() {
+        initializeFieldMap();
+    }
+    public ResultSet parseFrom(ResultSet resultSet, String... fields) {
+        try {
+            resultSet.next();
+            for (int i = 0; i < fields.length; i++) {
+                switch (fieldIndexMap.get(fields[i])) {
+                    case 0:
+                        reviewID = resultSet.getInt(i);
+                        break;
+                    case 1:
+                        rating = resultSet.getFloat(i);
+                        break;
+                    case 2:
+                        text = resultSet.getString(i);
+                        break;
+                    case 3:
+                        author = resultSet.getString(i);
+                        break;
+                    case 4:
+                        date = resultSet.getDate(i);
+                        break;
+                    case 5:
+                        songID = resultSet.getInt(i);
+                }
+            }
+        } catch (SQLException e) {
+            //TODO handle
+        }
+        return resultSet;
+    }
+
 }
